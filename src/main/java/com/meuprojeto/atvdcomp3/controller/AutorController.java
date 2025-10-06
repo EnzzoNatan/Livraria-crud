f7c1cbbdcfcf2e37bcd05cd9ffc38db360433ad9
EnzzoNatan <enzzoferrazz1909@gmail.com>
2025-10-06 13:55:43 -0300

    implementacao completa de todos os controllers,services e repositorys

--- src/main/java/com/meuprojeto/atvdcomp3/controller/AutorController.java
+++ src/main/java/com/meuprojeto/atvdcomp3/controller/AutorController.java
@@ -1,13 +1,48 @@
 package com.meuprojeto.atvdcomp3.controller;
 
+import com.meuprojeto.atvdcomp3.model.Autor;
+import com.meuprojeto.atvdcomp3.service.AutorService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
 public class AutorController {
 
+    private final AutorService autorService;
+
+    @PostMapping
+    public ResponseEntity<Autor> criarAutor(@RequestBody Autor autor){
+        autorService.criarAutor(autor);
+        return ResponseEntity.ok(autor);
+    }
+
+    @GetMapping
+    public ResponseEntity<Autor> consultarAutorPorId(@RequestParam Integer id){
+        return ResponseEntity.ok(autorService.consultarAutorPorId(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Autor>> consultarTodosAutores(){
+        return ResponseEntity.ok(autorService.consultarTodosAutores());
+    }
+
+    @PutMapping
+    public ResponseEntity<Autor> atualizarAutores(@PathVariable Integer id, @RequestBody Autor autor){
+        Autor autorAtualizado = autorService.atualizarAutores(id, autor);
+        return ResponseEntity.ok(autorAtualizado);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Autor> deletarAutor(@PathVariable Integer id){
+        autorService.deletarAutor(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/com/meuprojeto/atvdcomp3/service/AutorService.java
+++ src/main/java/com/meuprojeto/atvdcomp3/service/AutorService.java
@@ -1,4 +1,53 @@
 package com.meuprojeto.atvdcomp3.service;
 
+import com.meuprojeto.atvdcomp3.model.Autor;
+import com.meuprojeto.atvdcomp3.model.Editora;
+import com.meuprojeto.atvdcomp3.repository.AutorRepository;
+import com.meuprojeto.atvdcomp3.repository.EditoraRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AutorService {
+
+    private final AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository){
+        this.autorRepository = autorRepository;
+    }
+
+    public void criarAutor(Autor autor){
+        autorRepository.saveAndFlush(autor);
+    }
+
+    public Autor consultarAutorPorId(Integer id){
+        return autorRepository.findById(id).orElse(null);
+    }
+
+    public List<Autor> consultarTodosAutores(){
+        return autorRepository.findAll();
+    }
+
+    public Autor atualizarAutores(Integer id, Autor autor){
+        Optional<Autor> autorExistente = autorRepository.findById(id);
+        if(autorExistente.isPresent()){
+
+            autor.setNome(autor.getNome());
+            autor.setEmail(autor.getEmail());
+            autor.setNacionalidade(autor.getNacionalidade());
+
+            return autorRepository.save(autor);
+        } else {
+            throw new RuntimeException("Autor com id" + id + " nao encontrado");
+        }
+    }
+
+    public void deletarAutor(Integer id){
+        autorRepository.deleteById(id);
+    }
+
+
+
 }
